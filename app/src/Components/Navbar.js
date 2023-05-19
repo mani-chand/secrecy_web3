75b807cf52bee099da84a9d0fa8dd53b63b6c996
Manichand Kondapaka <65899331+mani-chand@users.noreply.github.com>
2023-05-19 21:53:36 +0530

    Update Navbar.js

--- app/src/Components/Navbar.js
+++ app/src/Components/Navbar.js
@@ -1,23 +1,41 @@
-import {Grid,Button,Typography} from '@mui/material/';
-const NavBar = ({connectToMetaMask}) => {
-    return ( 
+import { Grid, Button, Typography } from "@mui/material/";
+const NavBar = ({ connectToMetaMask }) => {
+  return (
     <>
-    <Grid container direction="row" spacing={2}>
-  <Grid item xs={10}>
-  <Typography variant="h4" gutterBottom>Secreacy </Typography>
-  </Grid>
-  <Grid item xs={2}>
-  <Button
-          style={{ marginTop: "10px" }}
-          onClick={connectToMetaMask}
-          size="medium"
-          variant="contained"
-        >
-          connect to metamask
-        </Button>
-  </Grid>
-</Grid>
+      <Grid container direction="row" spacing={2}>
+        <Grid item xs={8}>
+          <Typography variant="h4" gutterBottom>
+            Secreacy{" "}
+          </Typography>
+        </Grid>
+        <Grid item xs={4}>
+          <Button
+            style={{ margin: "10px" }}
+            href="/"
+            size="medium"
+            variant="text"
+          >
+            Home
+          </Button>
+          <Button
+            style={{ margin: "10px" }}
+            size="medium"
+            href="/notes"
+            variant="text"
+          >
+            Notes
+          </Button>
+          <Button
+            style={{ margin: "10px" }}
+            onClick={connectToMetaMask}
+            size="medium"
+            variant="contained"
+          >
+            connect to metamask
+          </Button>
+        </Grid>
+      </Grid>
     </>
-);
-}
+  );
+};
 export default NavBar;
